4b550e086c8461ec3ba923de706c8679fa1f7d81
RyanTanMing <125744706+RyanTanMing@users.noreply.github.com>
2024-03-25 17:03:07 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 function setup() {
     canvas=createCanvas(450,450)
     canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+function preload() {
+
+    
+}
+function draw() {
+    image(video,0,0,450,450)
+    person=document.getElementById("person").value
+    console.log(person)
+}
+function start() {
+    obj_detector=ml5.objectDetector("cocossd",model_loaded)
+document.getElementById("status").innerHTML="status: detecting objects"
+}
+function model_loaded() {
+    console.log("model loaded")
+    flag=true
 }
